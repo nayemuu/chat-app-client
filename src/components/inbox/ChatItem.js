a5d21b4f3cd822e9bc51e8ab9e1d45e3a8abf1fd
Nayeem-Hasan <hasan@care-box.com>
2024-06-09 17:37:56 +0600

    working

--- src/components/inbox/ChatItem.js
+++ src/components/inbox/ChatItem.js
@@ -1,31 +1,34 @@
 import { Link } from "react-router-dom";
+import moment from 'moment';
 
 export default function ChatItem({conversation}) {
+    const localAuth = localStorage.getItem('auth');
+    const {email} = JSON.parse(localAuth);
+    console.log("email = ", email);
+    let partnar = conversation.creator.email === email ? conversation.participant : conversation.creator;
 
     let avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-    let name="Saad Hasan"
     let lastMessage="bye"
-    let lastTime="25 minutes"
     console.log("conversation = ", conversation);
 
 
     return (
         <Link
             className="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none"
-            to="/"
+            to={`/inbox/${conversation.id}`}
         >
             <img
                 className="object-cover w-10 h-10 rounded-full"
                 src={avatar}
-                alt={name}
+                alt={partnar.name}
             />
             <div className="w-full pb-2 hidden md:block">
                 <div className="flex justify-between">
                     <span className="block ml-2 font-semibold text-gray-600">
-                        {name}
+                        {partnar.name}
                     </span>
                     <span className="block ml-2 text-sm text-gray-600">
-                        {lastTime}
+                        {moment(conversation.updatedAt).fromNow()}
                     </span>
                 </div>
                 <span className="block ml-2 text-sm text-gray-600">
